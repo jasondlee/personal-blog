67c413ff52a826f3c56015180b0c5361aa46fd25
Jason Lee <jason@steeplesoft.com>
2013-04-11 14:49:54 -0500

    Copied Rakefile from the tubes Configure production deployment Rename truncate to readmore Add filter() to remove readmore marker Attempt to get a tag cloud working

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,4 +1,4 @@
-require 'truncate'
+require 'readmore'
 require 'erubis'
 require 'tilt'
  
@@ -8,8 +8,11 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/posts', :posts)
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page => 5 )
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/feed.atom', :feed_title=>'Steeplesoft' )
+  extension Awestruct::Extensions::TagCloud.new( :tagcloud, '/index')
+  extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/blog/tags', :per_page => 5 )
 
   helper Awestruct::Extensions::Partial
-  helper Awestruct::Extensions::Truncate
+  helper Awestruct::Extensions::ReadMore
 end
 
--- _ext/readmore.rb
+++ _ext/readmore.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 
 module Awestruct
   module Extensions
-    module Truncate
+    module ReadMore
       def truncate(content)
         delims = ["pass::[more]","pass::[<!-- more -->]","<!-- more -->","<!--more-->","&lt;!-- more --&gt;"]
         delims.each do |delim|
@@ -13,21 +13,11 @@ module Awestruct
                 end
             end
         end
-        #index = content.index("pass::[more]")
-        #if index == nil
-        #    index = content.index("<!-- more -->")
-        #    if index == nil
-        #      index = content.index("<!--more-->")
-        #    end
-        #end
-          
-        #if index != nil 
-        #  if index > -1
-        #    return content[0..index-1]
-        #  end
-        #end
         return content
       end 
+      def filter(content)
+        return content.gsub("pass::[more]", "")
+      end
     end
   end
 end
