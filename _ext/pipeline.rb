1cf1446b1769667a1fc1cbf59bcb3d1b883b23ee
Jason Lee <jason@steeplesoft.com>
2013-04-11 17:06:09 -0500

    Increase posts per page to 10

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -7,7 +7,7 @@ require 'tilt'
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/posts', :posts)
   extension Awestruct::Extensions::Indexifier.new
-  extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page => 5 )
+  extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page => 10 )
   extension Awestruct::Extensions::Atomizer.new( :posts, '/feed.atom', :feed_title=>'Steeplesoft' )
   extension Awestruct::Extensions::TagCloud.new( :tagcloud, '/index')
   extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/blog/tags', :per_page => 5 )
