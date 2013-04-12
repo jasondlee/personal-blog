bbb2eb5a32e637c1c5bd4d6c909bc506406931a3
Jason Lee <jason@steeplesoft.com>
2013-04-12 15:34:06 -0500

    Fix tag handling Add CV and link

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -6,13 +6,15 @@ require 'tilt'
 
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new( '/posts', :posts)
-  extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Paginator.new( :posts, '/index', :per_page => 10 )
+  extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/posts/tags', :per_page => 10)
+  extension Awestruct::Extensions::TagCloud.new( :tagcloud, '/posts/tags/index.html', :layout=>'base', :title=>'Tags')
+
+  extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Atomizer.new( :posts, '/feed.atom', :feed_title=>'Steeplesoft' )
-  extension Awestruct::Extensions::TagCloud.new( :tagcloud, '/index')
-  extension Awestruct::Extensions::Tagger.new( :posts, '/index', '/blog/tags', :per_page => 5 )
 
   helper Awestruct::Extensions::Partial
   helper Awestruct::Extensions::ReadMore
+  helper Awestruct::Extensions::GoogleAnalytics
 end
 
