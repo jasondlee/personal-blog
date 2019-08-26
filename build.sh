35e0cd1d6273769e729126379579d1e779849767
Jason Lee <jason@steeplesoft.com>
2019-08-26 13:09:47 -0500

    Change build from maven to bash

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SRC=src
+DEST=output
+BUILD="-b $SRC $DEST"
+SERVE=""
+
+while getopts cdns opt
+do
+    case "$opt" in
+        c) rm -rf $DEST ;;
+        d) DEPLOY=true ;;
+        n) BUILD="" ;;
+        s) SERVE="-s" ;;
+        \?) usage ;;
+    esac
+done
+
+
+set -x
+if [ -n "$BUILD" -o -n "$SERVE" ] ; then
+    jbake $BUILD $SERVE
+fi
+
+if [ "$DEPLOY" == "true" ] ; then
+    source site.properties
+    rsync -varuP --delete -e ssh $DEST/* ${BLOG_USERNAME}@${BLOG_HOST}:${BLOG_DIR}/
+fi
