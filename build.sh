4872a5d63f954d69a3503fcd1648cef7989a1098
Jason Lee <jason@steeplesoft.com>
2019-09-03 12:09:20 -0500

    Added usage instructions Updated pickjdk.sh post with pointer to original author

--- build.sh
+++ build.sh
@@ -5,6 +5,15 @@ DEST=output
 BUILD="-b $SRC $DEST"
 SERVE=""
 
+function usage() {
+    echo "`basename $0`: <option>"
+    echo "    -c : Clean output directory"
+    echo "    -d : Deploy to production site"
+    echo "    -n : Do NOT build"
+    echo "    -s : Serve local copy"
+    exit 0;
+}
+
 while getopts cdns opt
 do
     case "$opt" in
