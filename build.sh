ddfb7c883b0a8b14a8c6defea032a2941782946e
Jason Lee <jason@steeplesoft.com>
2021-05-05 10:20:47 -0500

    Update build script for jekyll

--- build.sh
+++ build.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 
+
 BASEDIR="$( cd "$(dirname "$0")" ; pwd -P )"
 SRC=src
-DEST=output
-BUILD="--reset -b $SRC $DEST"
+DEST=_site
+BUILD="build"
 SERVE=""
+CONFIG="_config.yml"
 REVEAL_VER=3.8.0
-
-function buildReveal() {
-  REPO=$1
-
-  git clone $1 preso
-  cd preso
-  GEMFILE=`find . -name Gemfile`
-  BASEDIR=`dirname $GEMFILE`
-  cd $BASEDIR
-
-  bundle exec asciidoctor-revealjs \
-    -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/$REVEAL_VER \
-    --destination-dir build \
-    index.ad
-
-  cp -r assets/* build/
-}
 
 function usage() {
     echo "`basename $0`: <option>"
@@ -39,16 +24,18 @@ do
         c) echo "Cleaning..." ; rm -rf $DEST ;;
         d) DEPLOY=true ;;
         n) BUILD="" ;;
-        s) SERVE="-s" ;;
+        s) SERVE="serve --incremental" 
+            CONFIG="$CONFIG,_dev_config.yml"
+            ;;
         \?) usage ;;
     esac
 done
 
 if [ -n "$BUILD" -o -n "$SERVE" ] ; then
-    jbake $BUILD $SERVE
+    jekyll $BUILD $SERVE --config $CONFIG
 fi
 
 if [ "$DEPLOY" == "true" ] ; then
     source site.properties
-    rsync -varuP --delete -e ssh $DEST/.htaccess $DEST/* ${BLOG_USERNAME}@${BLOG_HOST}:${BLOG_DIR}/
+    rsync -n -varuP --delete -e ssh $DEST/* ${BLOG_USERNAME}@${BLOG_HOST}:${BLOG_DIR}/
 fi
