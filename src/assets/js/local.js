380d36da58f96fe64ae85d6657e2633cbed487de
Jason Lee <jason@steeplesoft.com>
2022-05-17 17:18:26 -0500

    More cleanup

--- src/assets/js/local.js
+++ src/assets/js/local.js
@@ -2,7 +2,7 @@ var index = Math.floor(Math.random() * quotes.length)
 document.getElementById("quote").innerHTML = (quotes[index].quote);
 document.getElementById("source").innerHTML = "-- " + (quotes[index].source);
 
-$(document).foundation()
-$(".image").each(function() {
-    $(this).attr("rel","lightbox[group]");
-});
+// $(document).foundation()
+// $(".image").each(function() {
+//     $(this).attr("rel","lightbox[group]");
+// });
