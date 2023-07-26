f8729b693d852b627b10fc7cd1502d7526bc8847
Jason Lee <jason@steeplesoft.com>
2023-07-26 15:37:17 -0500

    Remove lightbox

--- src/assets/js/local.js
+++ src/assets/js/local.js
@@ -1,8 +1,3 @@
 var index = Math.floor(Math.random() * quotes.length)
 document.getElementById("quote").innerHTML = (quotes[index].quote);
 document.getElementById("source").innerHTML = "-- " + (quotes[index].source);
-
-// $(document).foundation()
-// $(".image").each(function() {
-//     $(this).attr("rel","lightbox[group]");
-// });
