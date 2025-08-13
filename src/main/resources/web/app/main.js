75fa75e9524c2dc616d1c9bab3977faa0a9add32
Jason Lee <jason@steeplesoft.com>
2025-08-12 19:36:50 -0500

    Line number support
    Copy highlightjs-linenumbers.js from GH repo
      (https://raw.githubusercontent.com/wcoder/highlightjs-line-numbers.js/refs/heads/master/src/highlightjs-line-numbers.js)
    Modify JS to create function
    Import and call from main.js

--- src/main/resources/web/app/main.js
+++ src/main/resources/web/app/main.js
@@ -1,4 +1,9 @@
 import hljs from 'highlight.js';
+import foo from './highlightjs-line-numbers.js';
 import 'highlight.js/styles/agate.css';
 
+
 hljs.highlightAll();
+
+foo(hljs, window, document);
+hljs.initLineNumbersOnLoad();
