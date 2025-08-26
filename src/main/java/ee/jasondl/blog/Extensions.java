4707675d101363f3d1724d777add39dc00d7c031
Jason Lee <jason@steeplesoft.com>
2025-08-26 18:52:45 -0500

    New post
    Roq update
    Style updates

--- src/main/java/ee/jasondl/blog/Extensions.java
+++ src/main/java/ee/jasondl/blog/Extensions.java
@@ -1,32 +1,25 @@
 package ee.jasondl.blog;
 
-import static io.quarkiverse.roq.frontmatter.runtime.RoqTemplateAttributes.PAGE_PATH;
-import static io.quarkiverse.roq.frontmatter.runtime.RoqTemplateAttributes.PAGE_URL;
-import static io.quarkiverse.roq.frontmatter.runtime.RoqTemplateAttributes.SITE_PATH;
-import static io.quarkiverse.roq.frontmatter.runtime.RoqTemplateAttributes.SITE_URL;
-import static io.quarkiverse.roq.frontmatter.runtime.RoqTemplateAttributes.SOURCE_PATH;
-
-import io.quarkiverse.roq.frontmatter.runtime.RoqTemplateAttributes;
-import io.quarkiverse.roq.frontmatter.runtime.model.DocumentPage;
-import io.quarkiverse.roq.frontmatter.runtime.model.Site;
-import io.quarkiverse.roq.plugin.asciidoctorj.runtime.AsciidoctorJConverter;
-import io.quarkus.arc.Arc;
-import io.quarkus.arc.InstanceHandle;
-import io.quarkus.arc.impl.LazyValue;
 import io.quarkus.qute.TemplateExtension;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @TemplateExtension
 public class Extensions {
 
     public static String format(Date date, String format) {
         return new SimpleDateFormat(format).format(date);
+    }
+
+    public static String removeToc(String doc) {
+        Document parse = Jsoup.parse(doc);
+        parse.select("#toc").remove();
+
+        return parse.html();
     }
 
     public static String excerpt(String text, int limit) {
