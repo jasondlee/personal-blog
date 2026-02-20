8bbc14a2710ab187b6134f62da7212fb06c72982
Jason Lee <jason@steeplesoft.com>
2026-02-20 09:34:11 -0600

    Theme updates

--- public/js/local.js
+++ public/js/local.js
@@ -1,3 +1,3 @@
 const index = Math.floor(Math.random() * quotes.length)
 document.getElementById("quote").innerHTML = (quotes[index].quote);
-document.getElementById("source").innerHTML = "-- " + (quotes[index].source);
+document.getElementById("source").innerHTML = "— " + (quotes[index].source);
