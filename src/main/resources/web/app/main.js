12d71e11a93aa62e3e2d985a22477cedc41cd74f
Jason Lee <jason@steeplesoft.com>
2026-03-13 16:51:19 -0500

    Move nav css/js to bundle

--- src/main/resources/web/app/main.js
+++ src/main/resources/web/app/main.js
@@ -8,3 +8,18 @@ installLineNumbers(hljs, window, document);
 
 hljs.highlightAll();
 hljs.initLineNumbersOnLoad();
+
+// Mobile menu toggle
+$(document).ready(function() {
+    $('#mobile-menu-toggle').on('click', function(e) {
+        e.preventDefault();
+        $('#mobile-menu').toggleClass('active');
+        $(this).toggleClass('active');
+    });
+
+    // Close mobile menu when clicking on a link
+    $('#mobile-menu a').on('click', function() {
+        $('#mobile-menu').removeClass('active');
+        $('#mobile-menu-toggle').removeClass('active');
+    });
+});
