2d0f969571db808280957e9417db806ac2d1df99
Jason Lee <jason@steeplesoft.com>
2026-04-06 14:32:54 -0500

    CSS cleanup

--- src/main/resources/web/app/main.js
+++ src/main/resources/web/app/main.js
@@ -1,8 +1,6 @@
 import hljs from 'highlight.js';
 import installLineNumbers from './highlightjs-line-numbers.js';
 import 'highlight.js/styles/rainbow.css';
-// import './new.css';
-// import './about.js';
 
 installLineNumbers(hljs, window, document);
 
