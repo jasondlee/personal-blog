a967ac723e91f0c13f7c90a5013c5170e5945ef2
Jason Lee <jason@steeplesoft.com>
2026-05-13 11:09:33 -0500

    CSS/JS cleanup
    Remove unused js/css
    Combine files
    Remove duplicate class decls
    Default to Red Hat fonts

--- src/main/resources/web/app/main.js
+++ src/main/resources/web/app/main.js
@@ -1,5 +1,7 @@
+import './js/about.js';
+
 import hljs from 'highlight.js';
-import installLineNumbers from './highlightjs-line-numbers.js';
+import installLineNumbers from './js/highlightjs-line-numbers.js';
 import CopyButtonPlugin from 'highlightjs-copy';
 import 'highlightjs-copy/dist/highlightjs-copy.min.css';
 import 'highlight.js/styles/rainbow.css';
